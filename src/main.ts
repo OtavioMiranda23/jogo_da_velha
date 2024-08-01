987b28d1288c413aa2c89c6491ca4d25451363c2
otavio <otaviomiranda@usp.br>
2024-08-01 18:29:58 -0300

    fix: implementa tratativa de erros na classe main

--- src/main.ts
+++ src/main.ts
@@ -14,61 +14,93 @@ class Main {
         this.game = new Board();
         this.isCpuTimeToPlay = false;
     }
-    public showInstructions() {
+    private showInstructions() {
         console.log("Seja bem-vindo ao Jogo da Velha!!!")
         console.log("Para jogar, digite um número de 1 a 9 para inserir sua marcação =)")
+    }
+    private validateMove(move: number):void {
+        if(!this.game.isMoveValid(move - 1)) {
+            throw new Error("Erro: Movimento inválido. A casa selecionada já está ocupada.");
+        }
+        this.game.assignMove(move, true);
+        this.isCpuTimeToPlay = !this.isCpuTimeToPlay;
+    }
+    private validateInput(input: string):number {
+        const inputNormalize = parseInt(input);
+        if(isNaN(inputNormalize) || inputNormalize < 1 || inputNormalize > 9) {
+        
+            throw new Error("Erro: entrada inválida. A entrada precisa ser um número disponível entre 1 e 9. Tente novamente.");
+        }
+        return inputNormalize;
     }
     public  build():void {
         this.showInstructions();
         this.promptUser();
     }
-    public promptUser():void {
-        if(this.game.checkIsWin()) {
-            console.log("Vitória");
-            console.log(this.game.giveMessageWinner());
-            this.game.printTable();
-            this.rl.close();
-            return
-        }
-        if(this.game.checkIsDraw()) {
-            console.log("Empate!")
-            this.rl.close();
-            return
-        }
-
-        console.log(this.isCpuTimeToPlay ? "É a vez do CPU:" : "Sua vez:")
-        if(!this.isCpuTimeToPlay) {
-            this.game.printTable();
-        } 
-
-        const cpuPlay = new Cpu().genarateNumberPlay(this.game.getTable());
-        if(cpuPlay === null) {
-            console.error("Todas as jogadas já foram feitas.");
-            return;
-        }
-        if(this.isCpuTimeToPlay) {
-            this.game.assignMove(cpuPlay, false);
-            console.log("O CPU escolhe a casa: ", cpuPlay);
-            this.isCpuTimeToPlay = !this.isCpuTimeToPlay
-            this.promptUser();  // Chama a si mesma recursivamente após o processamento da jogada do usuário
-            return
-        }
-
-        this.rl.question("Qual a sua jogada? (1-9) ", (play) => {
-            const playNormalize = parseInt(play);
-            
-            if (isNaN(playNormalize) || playNormalize < 1 || playNormalize > 9) {
-                console.log("Entrada inválida. Tente novamente.");
-            } else if (!this.game.isMoveValid(playNormalize - 1)) {
-                console.log("Movimento inválido: Casa selecionada já está ocupada");
-            } else {
-                this.game.assignMove(playNormalize, true);
-
-                this.isCpuTimeToPlay = !this.isCpuTimeToPlay
+    private promptUser():void {
+        try {
+            if(this.game.checkIsWin()) {
+                console.log("Vitória");
+                console.log(this.game.giveMessageWinner());
+                this.game.printTable();
+                this.rl.close();
+                return
             }
+            if(this.game.checkIsDraw()) {
+                console.log("Empate!")
+                this.rl.close();
+                return
+            }
+    
+            console.log(this.isCpuTimeToPlay ? "É a vez do CPU:" : "Sua vez:")
+            if(!this.isCpuTimeToPlay) {
+                this.game.printTable();
+            } 
+    
+            try {
+                const cpuPlay = new Cpu().genarateNumberPlay(this.game.getTable());
+                if(this.isCpuTimeToPlay) {
+                    this.game.assignMove(cpuPlay, false);
+                    console.log("O CPU escolhe a casa: ", cpuPlay);
+                    this.isCpuTimeToPlay = !this.isCpuTimeToPlay
+                    this.promptUser();  // Chama a si mesma recursivamente após o processamento da jogada do usuário
+                    return
+                }
+            } catch (err) {
+                if(err instanceof Error) {
+                    console.error(err.message);
+                    this.rl.close();
+                }
+                
+                else {
+                    console.error("Ocorreu um erro desconhecido.");
+                    this.rl.close();
+    
+                }
+            }
+            
+            
+            this.rl.question("Qual a sua jogada? (1-9) ", (play) => {
+                try {
+                    const playValidated = this.validateInput(play);
+                    this.validateMove(playValidated);
+                } catch (err) {
+                    if(err instanceof Error) {
+                        console.error(err.message);
+                    }
+                    else {
+                        console.error("Ocorreu um erro desconhecido.");
+                    }
+                } 
 
-            this.promptUser();  // Chama a si mesma recursivamente após o processamento da jogada do usuário
-        });
+                this.promptUser();  // Chama a si mesma recursivamente após o processamento da jogada do usuário
+            });
+        } catch (err) {
+            console.error("Ocorreu um erro inesperado:", err);
+            this.rl.close();
+        }
+       
     }
+
 }
 new Main().build();
